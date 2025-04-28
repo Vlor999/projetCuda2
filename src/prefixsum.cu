124212c5f3bf54f254cfd769c6821efbcb6438c0
Willem Adnet <willem.adnet@grenoble-inp.org>
2025-04-28 19:55:16 +0200

    Multithtreading pixels

--- src/prefixsum.cu
+++ src/prefixsum.cu
@@ -17,10 +17,10 @@ PrefixSumBlending_GPU::~PrefixSumBlending_GPU()
 
 void PrefixSumBlending_GPU::setup(uint2 dimensions, uint32_t samples_per_pixel)
 {
-    uint32_t n_pixels = dimensions.x * dimensions.y;
+    uint32_t numberPixels = dimensions.x * dimensions.y;
 
     // example: allocate additional buffer (malloc not relevant for timing)
-    CUDA_CHECK_THROW(cudaMalloc(&_d_weights, sizeof(float) * n_pixels * samples_per_pixel));
+    CUDA_CHECK_THROW(cudaMalloc(&_d_weights, sizeof(float) * numberPixels * samples_per_pixel));
 
     CUDA_SYNC_CHECK_THROW();
 }
@@ -38,50 +38,91 @@ void PrefixSumBlending_GPU::run(DatasetGPU &data, float3 *d_img_out)
     this->reduceColor(data._n_pixels, data._samples_per_pixel, data._alphas, _d_weights, data._colors, d_img_out);
 }
 
-__global__ void prefixSumWeightsIteration(const float* alpha_in, float* weights_out, uint32_t n_samples_per_pixel) {
-    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
-    const float initVal = 1.0f;
+__global__ void prefixSumWeightsIteration(const float* alpha_in, float* weights_out, uint32_t numberPixels, uint32_t n_samples_per_pixel) {
+    const uint32_t pixel = blockIdx.x;
+    const uint32_t thread = threadIdx.x;
+    const uint32_t numberthreads = blockDim.x;
 
-    const float* alphas = alpha_in + pos * n_samples_per_pixel;
-    float* weights = weights_out + pos * n_samples_per_pixel;
+    if (pixel >= numberPixels){
+        return;
+    }
 
-    float acc = 1.0f;
+    extern __shared__ float sharedMemo[];
+    float *alpha = sharedMemo;
+    float *T = &sharedMemo[n_samples_per_pixel];
 
-    for (uint32_t i = 0; i < n_samples_per_pixel; ++i) {
-        weights[i] = acc;
-        acc *= (initVal - alphas[i]);
+    for (uint32_t i = thread; i < n_samples_per_pixel; i += numberthreads){
+        alpha[i] = alpha_in[pixel * n_samples_per_pixel + i];
+    }
+
+    __syncthreads();
+
+    if (thread == 0) {
+        float ti = 1.0f;
+        for (uint32_t i = 0; i < n_samples_per_pixel; ++i) {
+            T[i] = ti;
+            ti *= (1.0f - alpha[i]);
+        }
+    }
+
+    __syncthreads();
+
+    for (uint32_t i = thread; i < n_samples_per_pixel; i += numberthreads) {
+        weights_out[pixel * n_samples_per_pixel + i] = T[i];
     }
 }
 
-void PrefixSumBlending_GPU::prefixSumWeights(uint32_t n_pixels, uint32_t n_samples_per_pixel, const float *d_alpha_in, float *d_weights_out)
-{
-    // Equation : 2
-    uint32_t grid = (n_pixels + BLOCK - 1) / BLOCK;
-    prefixSumWeightsIteration<<<grid, BLOCK>>>(d_alpha_in, d_weights_out, n_samples_per_pixel);
+__global__ void reduceColorIteration(const float *alpha_in, const float *weights_in, const float3 *colors_in, float3 *img_out, uint32_t numberPixels, uint32_t n_samples_per_pixel){
+    uint32_t pixel = blockIdx.x;
+    if (pixel >= numberPixels){
+        return;
+    }
+    uint32_t thread = threadIdx.x;
+    uint32_t numberthreads = blockDim.x;
 
+
+    extern __shared__ float3 sharedMem[];
+    float3 sum = make_float3(0.0f, 0.0f, 0.0f);
+
+    for (uint32_t i = thread; i < n_samples_per_pixel; i += numberthreads) {
+        uint32_t idx = pixel * n_samples_per_pixel + i;
+        float ai = alpha_in[idx];
+        float ti = weights_in[idx];
+        float3 ci = colors_in[idx];
+        sum += ci * ai * ti;
+    }
+
+    sharedMem[thread] = sum;
+    __syncthreads();
+
+    for (uint32_t s = numberthreads / 2; s > 0; s >>= 1) {
+        if (thread < s){
+            sharedMem[thread] += sharedMem[thread + s];
+        }
+        __syncthreads();
+    }
+
+    if (thread == 0){
+        img_out[pixel] = sharedMem[0];
+    }
+}
+
+void PrefixSumBlending_GPU::prefixSumWeights(uint32_t numberPixels, uint32_t n_samples_per_pixel, const float *d_alpha_in, float *d_weights_out){
+    // Equation : 2
+    const uint32_t threads = BLOCK;
+    const uint32_t blocks = numberPixels;
+    size_t shared_mem_size = sizeof(float) * n_samples_per_pixel * 2;
+
+    prefixSumWeightsIteration<<<blocks, threads, shared_mem_size>>>(d_alpha_in, d_weights_out, numberPixels, n_samples_per_pixel);
     CUDA_SYNC_CHECK_THROW();
 }
 
-__global__ void reduceColorIteration(uint32_t n_pixels, uint32_t n_samples_per_pixel, const float *alpha, const float *weights, const float3 *colors, float3 *img_out) {
+void PrefixSumBlending_GPU::reduceColor(uint32_t numberPixels, uint32_t n_samples_per_pixel, const float *d_alpha_in, const float *d_weights_in, const float3 *d_colors_in, float3 *img_out){
     // Equation : 1
-    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    const uint32_t threads = BLOCK;
+    const uint32_t blocks = numberPixels;
+    size_t shared_mem_size = sizeof(float3) * threads;
 
-    float3 sum = make_float3(0.0f, 0.0f, 0.0f);
-    uint32_t base = pos * n_samples_per_pixel;
-    for (uint32_t i = 0; i < n_samples_per_pixel; ++i) {
-        float ai = alpha[base + i];
-        float ti = weights[base + i];
-        float3 ci = colors[base + i];
-        sum += ci * (ai * ti);
-    }
-    img_out[pos] = sum;
-}
-
-void PrefixSumBlending_GPU::reduceColor(uint32_t n_pixels, uint32_t n_samples_per_pixel, const float *d_alpha_in, const float *d_weights_in, const float3 *d_colors_in, float3 *d_img_out)
-{
-    // Equation : 1
-    uint32_t grid = (n_pixels + BLOCK - 1) / BLOCK;
-    reduceColorIteration<<<grid, BLOCK>>>(n_pixels, n_samples_per_pixel, d_alpha_in, d_weights_in, d_colors_in, d_img_out);
-
+    reduceColorIteration<<<blocks, threads, shared_mem_size>>>(d_alpha_in, d_weights_in, d_colors_in, img_out, numberPixels, n_samples_per_pixel);
     CUDA_SYNC_CHECK_THROW();
 }
